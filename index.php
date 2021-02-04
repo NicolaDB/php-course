fc6744f6fb2e04489a735858b7033e61c84e7bc5
NicolaDB <78536909+NicolaDB@users.noreply.github.com>
2021-02-04 18:40:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,15 @@
     //within the php tag, we can also insert values into our html
     $name = "PHP store";
     $credit = 1000;
-    echo "<h1>Hello world!</h1>";
-    echo "welcome to".$name."your credit amount is".$credit;
-
+    echo "Welcome back in ".$name."!";
+    echo "Your amount of credits are ".$credit;
+    $product['Computer'] = 750;
+    $product['Car'] = 15000;
+    $product['Iphone'] = 1000;
+    $product['Toaster'] = 75;
+    foreach ($product as $key => $value) {
+      echo "<p>the ".$key." costs ".$value."</p>";
+    }
      ?>
   </body>
 </html>
