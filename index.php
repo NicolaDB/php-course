1234627e0e4c10cfbe332d01a183f6f4b7fa557b
NicolaDB <78536909+NicolaDB@users.noreply.github.com>
2021-02-04 16:33:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <?php
     //using the two horwards slashes we create a comment in php
     //within the php tag, we can also insert values into our html
+    $name = "PHP store";
+    $credit = 1000;
     echo "<h1>Hello world!</h1>";
+    echo "welcome to".$name."your credit amount is".$credit;
+
      ?>
   </body>
 </html>
