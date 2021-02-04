bfb16cebea70af6a29a31ca2c92d67a851ca71cc
NicolaDB <78536909+NicolaDB@users.noreply.github.com>
2021-02-04 16:09:47 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP store</title>
+  </head>
+  <body>
+    <?php
+    //using the two horwards slashes we create a comment in php
+    //within the php tag, we can also insert values into our html
+    echo "<h1>Hello world!</h1>";
+     ?>
+  </body>
+</html>
